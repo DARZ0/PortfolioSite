3a4fd987e8d04868dff54049e49b2757695465dd
DARZ0 <dany.resendiz1@ymail.com>
2025-07-07 14:07:51 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function toggleMenu() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+function filterProjects() {
+    const input = document.getElementById('project-filter');
+    const filter = input.value.toLowerCase();
+    const ul = document.getElementById('projects-list');
+    const li = ul.getElementsByTagName('li');
+    for (let i = 0; i < li.length; i++) {
+        const text = li[i].innerText.toLowerCase();
+        li[i].style.display = text.includes(filter) ? '' : 'none';
+    }
+}
