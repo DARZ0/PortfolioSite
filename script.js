c2513149792c9ef38b761b314c468cbc5b1cc673
DARZ0 <dany.resendiz1@ymail.com>
2025-07-07 15:46:01 -0500

    script.js

--- script.js
+++ script.js
@@ -18,17 +18,3 @@ function filterProjects() {
     }
 }
 
-const button = document.getElementById('submit');
-button.addEventListener('click', function (event) {
-    event.preventDefault();
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const message = document.getElementById('message').value;
-
-    if (name && email && message) {
-        alert(`Thank you, ${name}! Your message has been sent.`);
-        document.getElementById('contact').reset();
-    } else {
-        alert('Please fill in all fields.');
-    }
-});
